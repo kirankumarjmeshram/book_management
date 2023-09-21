665c7d7f39d8a7f1fee3eae97aed67ff69fdcbab
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-09-21 22:51:21 +0530

    frntend added

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,6 @@
 const app = require('./App');
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === 'production') {
-  const __dirname = path.resolve();
-  app.use('/uploads', express.static('/var/data/uploads'));
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  );
-} else {
-  const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
-}
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
